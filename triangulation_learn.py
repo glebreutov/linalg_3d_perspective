763c81a64ebdc584cb3b6dd3dbd7f9ad53f06232
glebreutov <glebr@algotradinghub.com>
2017-06-21 23:53:24 +0300

    trying to solve with simple NN

--- triangulation_learn.py
+++ triangulation_learn.py
@@ -25,19 +25,19 @@ FEATURES = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
 def input_func():
     gen = data_gen(10)
     cols = {f: [] for f in FEATURES}
-    ycol = {"X": [], "Y": [], "Z": []}
+    ycol = [[], [], []]
 
     for x, y in gen:
         for n in range(0, len(FEATURES)):
             cols[FEATURES[n]].append(x[n])
-        ycol["X"].append(y[0])
-        ycol["Y"].append(y[1])
-        ycol["Z"].append(y[2])
+        ycol[0].append(y[0])
+        ycol[1].append(y[1])
+        ycol[2].append(y[2])
 
     feature_cols = {k: tf.constant(cols[k]) for k in FEATURES}
 
     #answer_cols = {k: tf.constant(ycol[k]) for k in ("X", "Y", "Z")}
-    answer_cols = tf.constant(ycol.values())
+    answer_cols = tf.constant(ycol)
 
     return feature_cols, answer_cols
 
@@ -49,6 +49,8 @@ feature_cols = [tf.contrib.layers.real_valued_column(k) for k in FEATURES]
 
 
 regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_cols,
-                                          hidden_units=[11, 11, 3])
+                                          hidden_units=[11, 45])
 
 regressor.fit(input_fn=input_func, steps=2000)
+# xa, ya = input_func()
+# regressor.fit(x=xa, y=ya, steps=2000)
